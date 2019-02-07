9f299d6c8489121b52a56307041173de289c646f
Eric Ford <eford@psu.edu>
2019-02-07 03:58:16 -0500

    init

--- test/test1.jl
+++ test/test1.jl
@@ -1,33 +1,10 @@
 using Test
 
-@testset "Testing solution to Exercise 1" begin
-
+@testset "Create tests for your project" begin
 
 @testset "Testing linear algebra results" begin
-   tol = 1e-4
-   @test maximum(abs.((wavelength_model_centered_v1(x_obs,result_linalg) .- wavelength_model_centered_v1(x_obs,poly_coeff_true))./wavelength_model_centered_v1(x_obs,poly_coeff_true) )) <= tol
-
+   @test 1 == 1
 end;
 
-@testset "Testing LsqFit results" begin
-   tol = 1e-8
-   @test maximum(abs.((wavelength_model_centered_v1(x_obs,result_linalg) .- wavelength_model_centered_v1(x_obs,result_lsqfit.param))./wavelength_model_centered_v1(x_obs,result_linalg) )) <= tol
-
-   @test maximum(abs.((wavelength_model_centered_v2(x_obs,result_linalg) .- wavelength_model_centered_v2(x_obs,result_lsqfit_v2.param))./wavelength_model_centered_v2(x_obs,result_linalg) )) <= tol
-
-   @test maximum(abs.((wavelength_model_centered_v2(x_obs,result_linalg) .- wavelength_model_centered_v2(x_obs,result_lsqfit_with_jacobian.param))./wavelength_model_centered_v2(x_obs,result_linalg) )) <= tol
-
-end;
-
-@testset "Testing Optim results" begin
-   tol = 1e-8
-   @test maximum(abs.((wavelength_model_centered_v2(x_obs,result_linalg) .- wavelength_model_centered_v2(x_obs,result_optim_neldermead.minimizer))./wavelength_model_centered_v2(x_obs,result_linalg) ))  <= tol
-   @test maximum(abs.((wavelength_model_centered_v2(x_obs,result_linalg) .- wavelength_model_centered_v2(x_obs,result_optim_conjgrad.minimizer))./wavelength_model_centered_v2(x_obs,result_linalg) ))  <= tol
-   @test maximum(abs.((wavelength_model_centered_v2(x_obs,result_linalg) .- wavelength_model_centered_v2(x_obs,result_optim_conjgrad_autograd.minimizer))./wavelength_model_centered_v2(x_obs,result_linalg) ))  <= tol
-   @test maximum(abs.((wavelength_model_centered_v2(x_obs,result_linalg) .- wavelength_model_centered_v2(x_obs,result_optim_bfgs.minimizer))./wavelength_model_centered_v2(x_obs,result_linalg) ))  <= tol
-   @test maximum(abs.((wavelength_model_centered_v2(x_obs,result_linalg) .- wavelength_model_centered_v2(x_obs,result_optim_bfgs_autodiff.minimizer))./wavelength_model_centered_v2(x_obs,result_linalg) ))  <= tol
-
-end;
-
-end; # Exercise 1
+end; # ex1.jl
 
